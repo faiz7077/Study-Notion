3beac21d9dfa66e1fb0bc00713b01230e8307a0f
Faizan Shaikh <75465806+faiz7077@users.noreply.github.com>
2025-08-17 23:53:17 +0530

    Update emailOtpTemplate.js

--- backend/mail/templates/emailOtpTemplate.js
+++ backend/mail/templates/emailOtpTemplate.js
@@ -7,6 +7,7 @@ const emailOtpTemplate = (otp) => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>OTP Verification Email</title>
 
+
     <style>
       body {
         background-color: #ffffff;
