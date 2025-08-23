e332c470e51cb2bde6de5d802be1b86dd957dc4f
Faizan Shaikh <75465806+faiz7077@users.noreply.github.com>
2025-08-23 23:44:40 +0530

    Update CourseDetails.jsx

--- frontend/src/pages/CourseDetails.jsx
+++ frontend/src/pages/CourseDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+
 import { useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom'
 import Spinner from '../components/common/Spinner';
